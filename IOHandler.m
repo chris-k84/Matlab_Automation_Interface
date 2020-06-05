7cee59afd77721cdd9c6fa1ba4540dba5b7ecc93
BAUKChrisK <c.knight@beckhoff.co.uk>
2020-06-05 22:20:46 +0100

    broke up scan ethercat masters function into smaller single responsible functions, added funtion to create an EC MAster

--- IOHandler.m
+++ IOHandler.m
@@ -18,121 +18,95 @@ classdef IOHandler < handle
         function Boxes = ScanEtherCatMasters(this, AmsNetId)
         % ScanEtherCatMasters scans the target system for EtherCAT masters
         %
-        %   Boxes = ScanEtherCatMasters(AmsNetId)
-        %   If the target system specified by its Net-ID (AmsNetId) is in
-        %   Config-Mode its scanned for EtherCAT masters. 
-        %   
-        %   set property: devices
-        %
-        %   see also:
-        %   <a href="https://infosys.beckhoff.com/content/1033/tc3_automationinterface/45035996516641675.html"
-        %   >Beckhoff Infosys</a>
+            this.Devices = {};
             Boxes = {};
-
-            if strcmpi(AmsNetId, 'Local')
-                AmsNetId = char(TwinCAT.Ads.AmsNetId.Local.ToString()); % here local
-            end
-
             if (this.ReadTwinCatState(AmsNetId) ~= TwinCAT.Ads.AdsState.Config)
                 error('target system is not in config mode, devices could not be scanned');                
             else
                 % change to Target systen with AmsNetId
                 this.sysManager.SetTargetNetId(AmsNetId);
 
-                pause(2) % wait for target
-
-                % get handle to devices item 
-                ioDevicesItem = this.sysManager.LookupTreeItem('TIID');
-
-                % xml work
-                scannedXml = ioDevicesItem.ProduceXml(false);
-                xmlDoc = System.Xml.XmlDocument;
-                xmlDoc.LoadXml(scannedXml);
-                xmlDeviceList = xmlDoc.SelectNodes('TreeItem/DeviceGrpDef/FoundDevices/Device');
-
-                this.devices = {};
-                ECDevicesFound = 0;
-                % get devices
-                for ii = 0:1:(xmlDeviceList.Count-1) % devices start with 0
-
-                    % get next device item
-                    node = xmlDeviceList.Item(ii);
-
-                    % get node specification
-                    typeName    = node.SelectSingleNode('ItemSubTypeName').InnerText;
-                    xmlAddress  = node.SelectSingleNode('AddressInfo');                                
-                    itemSubType = int32(str2num(char(node.SelectSingleNode('ItemSubType').InnerText)));
-                    
-                    % ignore devices that are not EtherCAT masters
-                    if(itemSubType == 111)
-                        % add found node to device tree
-                        ECDevicesFound = ECDevicesFound + 1;
-                        device      = ioDevicesItem.CreateChild(System.String.Format('Device_{0}_{1}',ii,typeName),itemSubType,'',{});                
-                        xml         = System.String.Format('<TreeItem><DeviceDef>{0}</DeviceDef></TreeItem>',xmlAddress.OuterXml);
-
-                        device.ConsumeXml(xml);
-
-                        this.devices = [this.devices {device}];
-                     
-                    end
-                end
-                X =[num2str(ECDevicesFound), ' EtherCAT Masters Found'];
-                disp(X)
+                this.Devices = GetDeviceMasters(sysManager);
+                
+                EcMasters = ScanForEcMasters(this.Devices);
+                
                 % scan for boxes
                 for ii = 1:1:(numel(this.devices)) 
-                    xml = '<TreeItem><DeviceDef><ScanBoxes>1</ScanBoxes></DeviceDef></TreeItem>';
-                    try
-                        disp(['---- ' char(this.devices{ii}.Name) ' ----'])
-                        this.devices{ii}.ConsumeXml(xml);                   
-                    catch e
-                        disp(e.message)
-                    end
+                    Boxes = GetEcNetworkBoxes(EcMasters(ii));
+                end
+                
+            end
+        end 
+        
+        function devices = GetDeviceMasters(this, sysMan)
+            % get handle to devices item 
+            ioDevicesItem = this.sysManager.LookupTreeItem('TIID');
 
-                    %for jj = 1:1:(this.devices{ii}.ChildCount) % childs of devices start with 1
-                    %    Boxes = [Boxes, char(this.devices{ii}.Child(jj).PathName)]; %#ok<AGROW>
-                    %    disp(['Found Box: ', char(this.devices{ii}.Child(jj).PathName)]) % use this for linking
-                    %end
-                    if this.devices{ii}.ChildCount == 0
-                        disp('No Terminals found.')
-                    else
-                        X = [num2str(this.devices{ii}.ChildCount),' Terminals Found'];
-                        disp(X)
-                    end
+            % xml work
+            scannedXml = ioDevicesItem.ProduceXml(false);
+            xmlDoc = System.Xml.XmlDocument;
+            xmlDoc.LoadXml(scannedXml);
+            xmlDeviceList = xmlDoc.SelectNodes('TreeItem/DeviceGrpDef/FoundDevices/Device');
+
+            devices = xmlDeviceList;
+        end
+        
+        function EcMasters = ScanForEcMasters(this, deviceList)
+            ECDevicesFound = 0;
+            % get devices
+            for ii = 0:1:(deviceList.Count-1) % devices start with 0
+
+                % get next device item
+                node = deviceList.Item(ii);
+
+                % get node specification
+                typeName    = node.SelectSingleNode('ItemSubTypeName').InnerText;
+                xmlAddress  = node.SelectSingleNode('AddressInfo');                                
+                itemSubType = int32(str2num(char(node.SelectSingleNode('ItemSubType').InnerText)));
+
+                % ignore devices that are not EtherCAT masters
+                if(itemSubType == 111)
+                    % add found node to device tree
+                    ECDevicesFound = ECDevicesFound + 1;
+                    device      = ioDevicesItem.CreateChild(System.String.Format('Device_{0}_{1}',ii,typeName),itemSubType,'',{});                
+                    xml         = System.String.Format('<TreeItem><DeviceDef>{0}</DeviceDef></TreeItem>',xmlAddress.OuterXml);
+
+                    device.ConsumeXml(xml);
+
+                    EcMasters = [EcMasters {device}];
+
                 end
             end
-        end %needs rewriting into class
+        end
+        
+        function EtherCATBoxes = GetEcNetworkBoxes(this, device)
+            xml = '<TreeItem><DeviceDef><ScanBoxes>1</ScanBoxes></DeviceDef></TreeItem>';
+            try
+                this.devices.ConsumeXml(xml);                   
+            catch e
+                disp(e.message);
+            end
+            for jj = 1:1:(this.devices.ChildCount) % childs of devices start with 1
+                EtherCATBoxes = [Boxes, char(this.devices{ii}.Child(jj).PathName)];
+                %disp(['Found Box: ', char(this.devices{ii}.Child(jj).PathName)]) % use this for linking
+            end
+        end
         
         function EtherCATMaster = CreateEtherCAT(this)
             this.Devices = this.sysManager.LookupTreeItem("TIID");
             this.EcMaster = this.Devices.CreateChild("EtherCAT Master", 111, '', {});
-        end %need to retuern EcMaster ref
-        function this = AddEcSlave(this)%rewrite this to add slave 1 at a time
-            ek1100 = this.EcMaster.CreateChild('EK1100', 9099, '', 'EK1100-0000-0001');
+            EtherCATMaster = this.EcMaster;
+        end 
+        
+        function Child = AddEcSlave(this, ParentNode)%rewrite this to add slave 1 at a time
+            ek1100 = ParentNode.CreateChild('EK1100', 9099, '', 'EK1100-0000-0001');
             el1004 = ek1100.CreateChild('EL1004', 9099, '', 'EL1004-0000-0000');
-            xml = el1004.ProduceXml();
-            this.xmlDoc = System.Xml.XmlDocument;
-            this.xmlDoc.LoadXml(xml);
-            % get element in xml by tag name
-            %itemName = this.xmlDoc.GetElementsByTagName("ItemName");
-            % change inner text of tag
-            %%if itemName ~= 0
-            %    itemName.setAttribute('ChangedByAi');
-            %end
-            item = this.xmlDoc.GetElementsByTagName('Name')
-            thislist = item.item(0);
-            thislist.SetAttribute('Name','Test');
-            
-            el1004.ConsumeXml(this.xmlDoc.InnerXml);
         end
          
         function CanDevice = AddCanInterface(this)
             this.Devices = this.sysManager.LookupTreeItem('TIID');
             this.CAN = this.Devices.CreateChild("CANDevice", 87, '', {});
-            %CanDevice = this.CAN.CreateChild("CAN", 5051, '', '');
             CanDevice = this.CAN.ImportChild(['C:\Users\chrisk\Desktop\', 'Box 27 (CAN Interface).xti'],'',true,'CanDevice-1');
-            CanDeviceXml = CanDevice.ProduceXml(false);
-            this.xmlDoc = System.Xml.XmlDocument;
-            this.xmlDoc.LoadXml(CanDeviceXml);
         end
     end
 end
